938f935ab7c0aa35324249cbb7d28d6fa8a2cab9
hicham terfani <h.terfani4237@uca.ac.ma>
2026-03-06 11:33:52 +0000

    add exercice1.js

---
+++ exercice1.js
@@ -0,0 +1,14 @@
+let a = 42;
+let b = "bonjour";
+let c = true;
+let d = null;
+let e;
+let f = [1 , 2 , 3 ];
+let g = { nom : "ali" };
+let h = NaN;
+
+console.log(a, "-->"  , typeof a);
+console.log(b, "-->" , typeof b);
+console.log(c, "-->", typeof b);
+console.log(d, )
+
